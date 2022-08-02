9b3594ae0c1b2afef4f89ec6e7fd1729652846f7
Akons15 <atanaszahov39@gmail.com>
2022-08-02 17:43:59 +0300

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [currency, setCurrency] = useState('usd')
+  const [currencyList, setCurrencyList] = useState([])
+
+  useEffect(() => {
+    getCurrencyData()
+  },[currency])
+
+  const getCurrencyData = async () => {
+    try {
+      const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+      const data = await response.json()
+      setCurrencyList(data.usd || data.eur || data.bgn || data.aud || 
+        data.cad || data.chf || data.nzd)
+    } catch(error) {
+      console.log(error)
+    }
+  } 
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <div>
+        <select onChange={e => setCurrency(e.target.value)}>
+          <option value="usd" >USD</option>
+          <option value="eur" >EUR</option>
+          <option value="aud" >AUD</option>
+          <option value="cad" >CAD</option>
+          <option value="chf" >CHF</option>
+          <option value="nzd" >NZD</option>
+          <option value="bgn" >BGN</option>
+        </select>
+      </div>
+
+      <div>
+      <h1>Group 1</h1>
+      {Object.entries(currencyList)
+      .filter(([key]) => key === 'eur' || key === 'bgn' || key === 'aud' ||  
+      key === 'chf' || key === 'nzd' || key === 'cad' || key === 'usd')
+      .map(([key, value, index]) => {
+        const values = value.toFixed(1)
+        return (
+          <div key={index}>
+          {values < 1 &&
+          <div>
+            {currency.toLocaleUpperCase()}-{key.toUpperCase()} : {values}
+          </div>
+          }
+        </div>
+          )
+      })}
+      </div>
+
+      <div>
+      <h1>Group 2</h1>
+      {Object.entries(currencyList)
+      .filter(([key]) => key === 'eur' || key === 'bgn' || key === 'aud' ||  
+      key === 'chf' || key === 'nzd' || key === 'cad' || key === 'usd')
+      .map(([key, value, index]) => {
+        const values = value.toFixed(1)
+        return (
+          <div key={index}>
+          {values >= 1 &&  values < 1.5 &&
+          <div>
+          {currency.toLocaleUpperCase()}-{key.toUpperCase()} : {values}
+          </div>
+          }
+        </div>
+          )
+      })}
+      </div>
+
+      <div>
+      <h1>Group 3</h1>
+      {Object.entries(currencyList)
+      .filter(([key]) => key === 'eur' || key === 'bgn' || key === 'aud' ||  
+      key === 'chf' || key === 'nzd' || key === 'cad' || key === 'usd')
+      .map(([key, value, index]) => {
+        const values = value.toFixed(1)
+        return (
+          <div key={index}>
+          {values >= 1.5 &&
+          <div>
+          {currency.toLocaleUpperCase()}-{key.toUpperCase()} : {values}
+          </div>
+          }
+        </div>
+          )
+      })}
+      </div>
     </div>
   );
 }
